372c47283a1b6f06a0dd7bc700b088ae0c8de019
Xiangyu Alaric Wei <35881947+AECassAWei@users.noreply.github.com>
2020-02-18 02:30:25 -0800

    lab06, project03 update

--- labs/lab06/lab06.py
+++ labs/lab06/lab06.py
@@ -43,7 +43,17 @@ def extract_book_links(text):
     >>> out[1] == url
     True
     """
-    return ...
+    soup = bs4.BeautifulSoup(text, 'lxml')
+    urls = []
+    for book in soup.find_all('article', attrs={'class':'product_pod'}):
+        # title = book.find('h3').a.get('title') # Title of the book
+        url = book.find('h3').a.get('href') # Url of the book
+        rating = book.find('p', attrs={'class':'star-rating'}).get('class')[1] # Rating of the book
+        price = float(book.find('p', attrs={'class':'price_color'}).text.strip('£')) # Price of the book
+
+        if (rating in ['Four', 'Five']) and (price < 50): # Rating at least four star, price less than £50
+            urls.append(url)
+    return urls # Answer
 
 
 def get_product_info(text, categories):
@@ -58,7 +68,40 @@ def get_product_info(text, categories):
     >>> out['Rating']
     'Two'
     """
-    return ...
+    soup = bs4.BeautifulSoup(text, 'lxml')
+    headers = ['Availability', 'Category', 'Description', 'Number of reviews', 'Price (excl. tax)', 'Price (incl. tax)', 'Product Type', 'Rating', 'Tax', 'Title', 'UPC'] # Headers of the dictionary
+
+    # Type, Category and Title
+    gens = soup.find('body', attrs={'id':'default'}).find('div', attrs={'class':'container-fluid'}).find_all('li')
+    ptype, category, title = gens[1].text.strip(), gens[2].text.strip(), gens[3].text.strip() # Get product type, category, title
+    if category not in categories: # Book not what we want
+            return None
+
+    # Availability, Description, Number of reviews, Price, Tax, Rating, UPC
+    article = soup.find('article', attrs={'class':'product_page'})
+    rating = article.find('p', attrs={'class':'star-rating'}).get('class')[1].strip() # Rating of the book
+
+    desrc_head = article.find('div', attrs={'class':'sub-header', 'id':'product_description'})
+    description = desrc_head.findNext('p').text.strip() # Decription
+
+    infos = article.find('table', attrs={'class':['table', 'table-striped']})
+    infos_head = infos.find_all('th')
+    for head in infos_head: # Loop through headers
+        if head.text == 'UPC': # UPC
+            upc = head.findNext('td').text.strip()
+        elif head.text == 'Price (excl. tax)': # Price (excl. tax)
+            pri_ex_tax = head.findNext('td').text.strip()
+        elif head.text == 'Price (incl. tax)': # Price (incl. tax)
+            pri_in_tax = head.findNext('td').text.strip()
+        elif head.text == 'Tax': # Tax
+            tax = head.findNext('td').text.strip()
+        elif head.text == 'Availability': # Availability
+            avail = head.findNext('td').text.strip()
+        elif head.text == 'Number of reviews': # Number of reviews
+            no_rev = head.findNext('td').text.strip()
+
+    content = [avail, category, description, no_rev, pri_ex_tax, pri_in_tax, ptype, rating, tax, title, upc]
+    return dict(zip(headers, content))
 
 
 def scrape_books(k, categories):
@@ -76,8 +119,35 @@ def scrape_books(k, categories):
     >>> out['Title'][0] == 'Sharp Objects'
     True
     """
-    return ...
+    df = pd.DataFrame() # Contain the result
+    url = 'http://books.toscrape.com/'
+    web = requests.get(url)
+    for _ in range(k):
+        # print(_)
+        url_text = web.text # Get text of page
+        book_links = extract_book_links(url_text) # Get book_links at least four star, price less than 50
 
+        if len(book_links) != 0: # If no book of four/five star, and price less than 50
+            for link in book_links: # Loop through book links
+                if link[0:10] != 'catalogue/': # Fix no found urls
+                    link = 'catalogue/' + link
+                # print(url + link)
+                book_text = requests.get(url + link).text
+                dic = get_product_info(book_text, categories)
+                df = df.append(dic, ignore_index=True) # Append met book to df
+
+        # Get next page url
+        soup = bs4.BeautifulSoup(url_text, 'html.parser')
+        nxt = soup.find('li', attrs={'class':'next'}) # Find next button section
+        if nxt == None: # Last page, no next url
+            continue
+        
+        app = nxt.find('a').get('href') # Next page url appendant
+        if app[0:10] != 'catalogue/': # Fix no found urls
+            app = 'catalogue/' + app
+        print(app)
+        web = requests.get(url + app)
+    return df
 
 # ---------------------------------------------------------------------
 # Question 3
@@ -94,7 +164,18 @@ def stock_history(ticker, year, month):
     >>> history.label.iloc[0]
     'June 03, 19'
     """
-    return ...
+    url = 'https://financialmodelingprep.com/api/v3/historical-price-full/' + ticker
+    response = requests.get(url)
+    data = response.text
+    js = json.loads(data) # Load json data
+    
+    # Construct dataframe from js file
+    df = pd.DataFrame()
+    for day in js.get('historical'):
+        df = df.append(day, ignore_index=True, sort=False)
+    # df = df.assign(year=pd.to_datetime(df['date']).dt.year)
+    # df = df.assign(month=pd.to_datetime(df['date']).dt.month)
+    return df[(pd.to_datetime(df['date']).dt.year == year) & (pd.to_datetime(df['date']).dt.month == month)]
 
 
 def stock_stats(history):
@@ -111,7 +192,17 @@ def stock_stats(history):
     >>> float(stats[1][:-1]) > 1
     True
     """
-    return ...
+    open_id = pd.to_datetime(history['date']).idxmin()
+    close_id = pd.to_datetime(history['date']).idxmax()
+    close_p = history.loc[close_id, 'close'] # Close price for month
+    open_p = history.loc[open_id, 'open'] # Opening price for month
+    pct_change = (close_p - open_p) / open_p * 100
+
+    tot_tran = ((history['close'] + history['open']) / 2 * history['volume'] / 1000000000).sum()
+    
+    change = '%+.2f' % (pct_change) + '%'
+    tran = '%.2f' % (tot_tran) + 'B'
+    return (change, tran)
 
 
 # ---------------------------------------------------------------------
@@ -129,7 +220,52 @@ def get_comments(storyid):
     >>> out.loc[5, 'time'].day
     31
     """
-    return ...
+    news_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + str(storyid) + '.json'
+    news = json.loads(requests.get(news_endpoint).text) # Story info
+    kids = news.get('kids') # Kids of story
+    
+    ids = get_kids_ids(kids, [])
+    df = pd.DataFrame()
+    cols = ['id', 'by', 'parent', 'text', 'time']
+    for idx in ids: 
+        kids_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + str(idx) + '.json'
+        com = json.loads(requests.get(kids_endpoint).text)
+
+        by = com.get('by') # Author
+        parent = com.get('parent') # Parent id
+        text = com.get('text') # Comment text
+        time = pd.to_datetime(com.get('time')) # Time of comment # How to change it to datetime??????????????????????????????
+
+        content = [idx, by, parent, text, time] # Content list
+        comment = dict(zip(cols, content)) # Zip into dataframe
+        df = df.append(comment, ignore_index=True) # Append to answer
+    return df
+
+
+# Helper function to get kids ids with recursion
+def get_kids_ids(kids, ids):
+    """
+    Get all the kids ids within kids.
+    
+    :param kids: a list of kids ids
+    :param ids: id list to append to and return
+    :return: a list of kids ids (recursive)
+    """
+    for kid in kids: # Loop through to check if comment
+        kids_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + str(kid) + '.json'
+        info = json.loads(requests.get(kids_endpoint).text)
+        
+        if not info.get('dead'): # Not dead comment 
+            ids.append(info.get('id')) # Append to list ids
+            
+            if (info.get('kids') != None) and (info.get('type') == 'comment'): # Has kids, is comment
+                kids_ids = get_kids_ids(info.get('kids'), []) # Kids id list
+
+                if len(kids_ids) != 0: # Has kids
+                    for idx in kids_ids:
+                        ids.append(idx) # Append kids to answer
+
+    return ids
 
 
 # ---------------------------------------------------------------------
